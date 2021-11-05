db1c72a80634391c7800a4d9fcc1c81048f38109
xMadhavGupta9 <madhav.gupta_ug23@ashoka.edu.in>
2021-11-05 23:18:20 +0530

    carousel

---
+++ components/Carousel/Carousel.js
@@ -0,0 +1,62 @@
+import React from 'react'
+// import { Carousel } from 'react-bootstrap'
+import Image from 'next/image'
+import styles from './Carousel.module.css'
+
+const Carousel = () => {
+    return(
+        <div className={styles.carouselContainer}>
+            <div className={styles.card}>
+                <Image src="/images/carousel.png" width='64%' height='36%' layout="responsive" objectFit='contain' />
+            </div>
+        </div>
+    )
+}
+
+
+
+
+//https://www.youtube.com/watch?v=5RNAg4r_f4c
+//https://react-bootstrap.github.io/components/carousel/
+    //individual item intervals
+// const CarouselContainer = () => {
+//     return(
+//         <Carousel>
+//             <Carousel.Item interval={1000}>
+//                 <img
+//                     className="d-block w-100"
+//                     src="/images/carousel.png"
+//                     alt="First slide"
+//                 />
+//                 <Carousel.Caption>
+//                     <h3>First slide label</h3>
+//                     <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+//                 </Carousel.Caption>
+//             </Carousel.Item>
+//             <Carousel.Item interval={500}>
+//                 <img
+//                     className="d-block w-100"
+//                     src="/images/carousel.png"
+//                     alt="Second slide"
+//                 />
+//                 <Carousel.Caption>
+//                     <h3>Second slide label</h3>
+//                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+//                 </Carousel.Caption>
+//             </Carousel.Item>
+//             <Carousel.Item>
+//                 <img
+//                     className="d-block w-100"
+//                     src="/images/carousel.png"
+//                     alt="Third slide"
+//                 />
+//                 <Carousel.Caption>
+//                     <h3>Third slide label</h3>
+//                     <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+//                 </Carousel.Caption>
+//             </Carousel.Item>
+//         </Carousel>
+//     )
+// }
+
+export default Carousel;
