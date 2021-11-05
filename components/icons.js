48ee6843ea3818dafe716b6d314ccca572d01b2c
Niranjan420 <niranjanrajesh02@gmail.com>
2021-11-06 05:18:07 +0530

    filter menu design complete

--- components/icons.js
+++ components/icons.js
@@ -173,3 +173,40 @@ export function ShoppingBag() {
     </svg>
   );
 }
+
+export function EmptyStar() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1.5"
+      color="var(--geist-foreground)"
+      shapeRendering="geometricPrecision"
+      viewBox="0 0 24 24"
+    >
+      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+    </svg>
+  );
+}
+export function FullStar() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      fill="currentColor"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1.5"
+      color="var(--geist-foreground)"
+      shapeRendering="geometricPrecision"
+      viewBox="0 0 24 24"
+    >
+      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+    </svg>
+  );
+}
--- pages/shop/index.js
+++ pages/shop/index.js
@@ -1,4 +1,4 @@
-import { ChevronDown, ChevronUp, ShoppingBag } from '@components/icons'
+import { ChevronDown, ChevronUp, EmptyStar, FullStar, ShoppingBag } from '@components/icons'
 import Dropdown from '@components/Navbar/Dropdown'
 import SortDropdown from '@components/SortDropdown/SortDropdown'
 import React, { useState } from 'react'
@@ -6,6 +6,15 @@ import styles from './shop.module.css'
 
 const Shop = () => {
   const [sortDropdown, setSortDropdown] = useState(false)
+  const [starHover, setStarHover] = useState(null)
+  function starHoverSetter(num) {
+    if (num === -1) {
+      setStarHover(null)
+    }
+    else {
+      setStarHover(num)
+    }
+  }
   return (
     <div className={styles.pageContainer}>
       <div className={styles.header}>
@@ -23,6 +32,110 @@ const Shop = () => {
         </div>
       </div>
       <div className={styles.body}>
+        <div className={styles.filterSection}>
+          <h3>FILTERS</h3>
+          <div className={styles.priceSection}>
+            <h4>Price</h4>
+            <div className={styles.minMaxCont}><h5>Min:</h5><input type="number" min={0} max={9999} placeholder="" /></div>
+            <div className={styles.minMaxCont}><h5>Max:</h5><input type="number" min={100} max={99999} placeholder="" /></div>
+          </div>
+          <div className={styles.sortReviewSection} >
+            <h4>Ratings</h4>
+            <div className={styles.starsRow} onMouseEnter={() => starHoverSetter(5)} onMouseLeave={() => starHoverSetter(-1)}>
+              {starHover === 5 && (
+                <>
+                  <FullStar />
+                  <FullStar />
+                  <FullStar />
+                  <FullStar />
+                  <FullStar />
+                </>
+              )}
+              {!(starHover === 5) && (
+                <>
+                  <EmptyStar />
+                  <EmptyStar />
+                  <EmptyStar />
+                  <EmptyStar />
+                  <EmptyStar />
+                </>
+              )}
+
+            </div>
+            <div className={styles.starsRow} onMouseEnter={() => starHoverSetter(4)} onMouseLeave={() => starHoverSetter(-1)}>
+              {(starHover === 4) && (
+                <>
+                  <FullStar />
+                  <FullStar />
+                  <FullStar />
+                  <FullStar />
+                  <p>+</p>
+                </>
+              )}
+              {!(starHover === 4) && (
+                <>
+                  <EmptyStar />
+                  <EmptyStar />
+                  <EmptyStar />
+                  <EmptyStar />
+                  <p>+</p>
+                </>
+              )}
+            </div>
+            <div className={styles.starsRow} onMouseEnter={() => starHoverSetter(3)} onMouseLeave={() => starHoverSetter(-1)}>
+              {(starHover === 3) && (
+                <>
+                  <FullStar />
+                  <FullStar />
+                  <FullStar />
+                  <p>+</p>
+                </>
+              )}
+              {!(starHover === 3) && (
+                <>
+                  <EmptyStar />
+                  <EmptyStar />
+                  <EmptyStar />
+                  <p>+</p>
+                </>
+              )}
+            </div>
+            <div className={styles.starsRow} onMouseEnter={() => starHoverSetter(2)} onMouseLeave={() => starHoverSetter(-1)}>
+              {(starHover === 2) && (
+                <>
+                  <FullStar />
+                  <FullStar />
+                  <p>+</p>
+                </>
+              )}
+              {!(starHover === 2) && (
+                <>
+                  <EmptyStar />
+                  <EmptyStar />
+                  <p>+</p>
+                </>
+              )}
+            </div>
+            <div className={styles.starsRow} onMouseEnter={() => starHoverSetter(1)} onMouseLeave={() => starHoverSetter(-1)}>
+              {(starHover === 1) && (
+                <>
+                  <FullStar />
+                  <p>+</p>
+                </>
+              )}
+              {!(starHover === 1) && (
+                <>
+                  <EmptyStar />
+                  <p>+</p>
+                </>
+              )}
+            </div>
+
+          </div>
+        </div>
+        <div className={styles.gallerySection}>
+          <div>GALLERY</div>
+        </div>
 
       </div>
     </div>
